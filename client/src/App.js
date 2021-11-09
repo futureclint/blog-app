c6b0b48db27da4bc3b47855d89e07df93cf4da37
jckrft <jckrft@gmail.com>
2021-11-09 11:45:02 -0500

    switch to routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,18 @@ import Posts from './screens/Posts/Posts';
 import PostCreate from './screens/PostCreate/PostCreate';
 import PostEdit from './screens/PostEdit/PostEdit';
 import PostDetail from './screens/PostDetail/PostDetail';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app">
-      <Switch>
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route exact path="/posts" component={Posts} />
         <Route path="/add-post" component={PostCreate} />
         <Route exact path="/posts/:id/edit" component={PostEdit} />
         <Route exact path="/posts/:id" component={PostDetail} />
-      </Switch>
+      </Routes>
     </div>
   )
 }
